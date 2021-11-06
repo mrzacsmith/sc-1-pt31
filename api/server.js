2a24cb2eab384aef5b2b61bdb84499c72d590d77
Zac Smith <zrs3141592@gmail.com>
2021-11-05 19:50:17 -0600

    data route

--- api/server.js
+++ api/server.js
@@ -3,11 +3,15 @@ const helmet = require('helmet')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const dataRouter = require('../routes/data')
+
 const server = express()
 server.use(helmet())
 server.use(cors())
 server.use(morgan('dev'))
 server.use(express.json())
+
+server.use('/api/v1/data', dataRouter)
 
 server.get('/', (req, res) => {
   res.status(200).json({
